58854143de3bc26272eb72b3ac6467fb32b01531
irodr029 <ivan@Ivans-MacBook-Pro.local>
2015-01-15 18:44:21 -0500

    Draft finishing questions and correcting errors

--- Calculations.R
+++ Calculations.R
@@ -23,11 +23,11 @@ data1 <- select(data.1, date,smlo_vwret,smme_vwret,smhi_vwret,bilo_vwret,bime_vw
 data2 <- select(data.2, date = dateff, rf)
 
 #Create final data set of interest
-data <- inner_join(data1, data2, by="date")
+data <- merge(data1, data2)
 data.portfolio <- select(data, -date, -rf)
 
 write.csv(data, file="FinalData.csv")
-
+?cov()
 ### HW PROBLEMS ##
 
 #Initializing vectors and values of interest
@@ -36,10 +36,10 @@ i <- 1 + numeric(n) #creating unity vector
 
 #1a The 6x1 vector mu
 mu <- sapply(data.portfolio, mean)
-
+mu
 #1b The 6x6 covariance matrix
 V <- cov(data.portfolio)
-
+V
 
 #1c Find intermediate A,B,C,D
 A <- t(i) %*% solve(V) %*% mu
@@ -67,7 +67,7 @@ mu.p <- .035
 w.p <- g + h * mu.p
 
 #1j Find the wieghts, $w_{op}$ and the mean , $\mu_{op}$ for portfolio $p$'s zero beta portfolio
-mu.0p <- (D/C^2)/(mu.p - D/C) + A/C
+mu.0p <- (-D/C^2)/(mu.p - (A/C)) + A/C
 mu.0p
 
 #1k Find the regression beta of the first portfolio's return with respect to portfolio $p$.
@@ -75,7 +75,7 @@ mu.0p
 port_vwret <- as.matrix(data.portfolio) %*% w.p
 port_vwret <- data.frame(port_vwret)
 data <- tbl_df(cbind(data, port_vwret))
-
+w.p
 #Creating separate regression data table in percent instead of decimals 
 data.regression <- select(data, smlo_vwret, port_vwret) * 100
 head(data.regression, n=4)
@@ -83,7 +83,10 @@ head(data.regression, n=4)
 #Run regression
 reg <- lm(smlo_vwret ~ port_vwret, data=data.regression)
 results <- summary(reg)
-results$coef[1,2]
+beta <- results$coef[2,1]
+
+mu.expression <- mu.0p + beta * (mu.p - mu.0p)
+round(mu.expression, 5) == round(mu[1],5)
 
 #Standard Deviation and Plot
 v <- diag(V)
@@ -99,13 +102,37 @@ alpha <- seq(0,2,.01)
 return.eff = NULL
 std.eff = NULL
 
-for (i in 1:length(alpha)) {
-return.eff[i] = alpha[i] * mu.gl + (1 - alpha[i]) * mu.p
-weight.eff = alpha[i] * (g + h*mu.gl) + (1 - alpha[i]) * (g + h*mu.p)
-std.eff[i] = sqrt(diag(t(weight.eff) %*% V %*% weight.eff))
+for (j in 1:length(alpha)) {
+return.eff[j] = alpha[j] * mu.gl + (1 - alpha[j]) * mu.p
+weight.eff = alpha[j] * (g + h*mu.gl) + (1 - alpha[j]) * (g + h*mu.p)
+std.eff[j] = sqrt(diag(t(weight.eff) %*% V %*% weight.eff))
 }
 
 frontier.data <- data.frame(cbind(return.eff, std.eff))
 ggplot() + 
   geom_path(data=frontier.data, aes(x=std.eff, y=return.eff)) + 
   geom_point(aes(x=s, y=mu)) + theme_bw()
+
+#1o Tangent portfolio
+rf <- mean(data$rf)
+z <- solve(V) %*% (mu - rf)
+z.sum <- t(i) %*% z
+w_t <- 1/drop(z.sum) * z
+w_t
+data.portfolio
+
+mu.t <- mean(as.matrix(data.portfolio) %*% w_t)
+std.t <- sqrt(diag(t(w_t) %*% V %*% w_t))
+
+ggplot() + 
+  geom_path(data=frontier.data, aes(x=std.eff, y=return.eff)) + 
+  geom_point(aes(x=s, y=mu)) + 
+  geom_point(aes(x=std.t, y=mu.t)) +
+  theme_bw()
+
+#1p proportion in complete portfolio
+
+A = 3
+y <- (mu.t - rf)/(0.5 * A * std.t^2)
+y
+
